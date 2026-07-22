5f8ea94e2a98439701e895b49366619422363c57
Umair-Hikmat <umairhikmat@gmail.com>
2026-07-22 14:08:06 +0500

    Create version.py

---
+++ version.py
@@ -0,0 +1,9 @@
+"""
+Version and build information for Quiz Studio.
+"""
+
+__version__ = "1.0.0"
+__build__ = "2026.07.22"
+__author__ = "Quiz Studio Core Team"
+__app_name__ = "Quiz Studio Professional"
+__license__ = "Commercial"
